436ef79626b66ba9520a3f8780639d3305531979
Kunduz-im <imankulovakndz03@gmail.com>
2026-01-18 10:36:11 +0600

    January promo Kunduz.
    January Promotion1
    
    January Promotion2
    
    January Promotion3
    
    January Promotion4
    
    January Promotion5
    
    January Promotion6

---
+++ src/kg/sim_bank/promoution/JanuaryPromotion.java
@@ -0,0 +1,16 @@
+package kg.sim_bank.promoution;
+
+public class JanuaryPromotion {
+    public static void main(String[] args) {
+        System.out.println("January Promotion");
+        System.out.println("January Promotion123");
+        System.out.println("January Promotion1234");
+        System.out.println("January Promotion12345");
+        System.out.println("January Promotion123456");
+        System.out.println("January Promotion1234567");
+        System.out.println("January Promotion12345678");
+        System.out.println("January Promotion123456789");
+        System.out.println("January Promotion1234567890");
+        System.out.println("January Promotion123456789010");
+    }
+}
